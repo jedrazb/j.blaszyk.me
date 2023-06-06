b6e2eadd38cc4598982c82a8e6be962f7294bd0a
jedrazb <jedrazb@gmail.com>
2023-06-06 23:45:07 +0200

    [blog post] wip on bikepacking in tuscany post

--- scripts/generate_images.py
+++ scripts/generate_images.py
@@ -7,7 +7,7 @@ def read_images(relative_path):
     images = [f for f in listdir(relative_path) if isfile(join(relative_path, f))]
     images = sorted(images)
     out = ['./photos/' + img for  img in images]
-    return out
+    print(out)
 
 def read_images_2d(relative_path):
     imgs = read_images(relative_path)
@@ -16,5 +16,5 @@ def read_images_2d(relative_path):
 
 
 
-
-read_images_2d('content/through-the-lens/tenerife-pleasures/photos')
+read_images('content/blog/bikepacking-in-tuscany-italy/photos')
+# read_images_2d('content/through-the-lens/tenerife-pleasures/photos')
